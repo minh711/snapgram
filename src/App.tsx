0fdc8dc4feeedd39f38946b5ab136141d3656a92
minh711 <duyminhtruong711@gmail.com>
2024-01-14 22:44:03 +0700

    Apply the layouts.

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,23 @@ import SigninForm from './_auth/forms/SigninForm';
 import SignupForm from './_auth/forms/SignupForm';
 import { Home } from './_root/pages';
 import './globals.css';
+import Authlayout from './_auth/Authlayout';
+import RootLayout from './_root/RootLayout';
 
 const App = () => {
   return (
     <main className="flex h-screen">
       <Routes>
         {/* public routes */}
-        <Route path="/sign-in" element={<SigninForm />} />
-        <Route path="/sign-in" element={<SignupForm />} />
+        <Route element={<Authlayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-in" element={<SignupForm />} />
+        </Route>
 
         {/* public routes */}
-        <Route index element={<Home />} />
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </main>
   )
