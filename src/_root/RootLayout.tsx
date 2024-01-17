3384c01802cc95b5ba13afa8d487ba8b2c064c22
minh711 <duyminhtruong711@gmail.com>
2024-01-17 22:03:19 +0700

    Tidy up.

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -6,14 +6,12 @@ import { Outlet } from 'react-router-dom'
 
 const RootLayout = () => {
   return (
-    <div className="w-full md:flex">
+    <div className='w-full md:flex'>
       <Topbar />
       <LeftSidebar />
-
-      <section className="flex flex-1 h-full">
+      <section className='flex flex-1 h-full'>
         <Outlet />
       </section>
-
       <Bottombar />
     </div>
   )
