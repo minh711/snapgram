b8c315aedec8d076a67dfb57f03f0c7b123c3500
minh711 <duyminhtruong711@gmail.com>
2024-01-17 22:03:47 +0700

    Tidy up.

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -4,8 +4,8 @@ import {
   useQueryClient,
   useInfiniteQuery,
 } from '@tanstack/react-query';
-import { INewUser } from "@/types";
-import { createUserAccount, signInAccount, signOutAccount } from "../appwrite/api";
+import { INewUser } from '@/types';
+import { createUserAccount, signInAccount, signOutAccount } from '../appwrite/api';
 
 export const useCreateUserAccount = () => {
   return useMutation({
