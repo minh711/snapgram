5d38f2fc82d461c4c08cfd34d837d9f43a3671a6
minh711 <duyminhtruong711@gmail.com>
2024-01-17 22:03:35 +0700

    Tidy up.

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -14,27 +14,25 @@ const Topbar = () => {
   }, [isSuccess]);
 
   return (
-    <section className="topbar">
-      <div className="flex-between py-4 px-5">
-        <Link to="/" className="flex gap-3 items-center">
+    <section className='topbar'>
+      <div className='flex-between py-4 px-5'>
+        <Link to='/' className='flex gap-3 items-center'>
           <img
-            src="/assets/images/logo.svg"
-            alt="logo"
+            src='/assets/images/logo.svg'
+            alt='logo'
             width={130}
             height={325}
           />
         </Link>
-
-        <div className="flex gap-4">
-          <Button variant="ghost" className="shad-button_ghost"
+        <div className='flex gap-4'>
+          <Button variant='ghost' className='shad-button_ghost'
             onClick={() => signOut()}>
-            <img src="/assets/icons/logout.svg" alt="logout" />
+            <img src='/assets/icons/logout.svg' alt='logout' />
           </Button>
-
-          <Link to={`/profile/${user.id}`} className="flex-center gap-3">
+          <Link to={`/profile/${user.id}`} className='flex-center gap-3'>
             <img
               src={user.imageUrl || '/assets/images/profile-placeholder.svg'}
-              alt="profile"
+              alt='profile'
               className='h-8 w-8 rounded-full'
             />
           </Link>
