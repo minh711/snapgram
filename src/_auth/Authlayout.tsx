65d3e4ae2dcc43ec3bba03c5eec47b64d5b1b31f
minh711 <duyminhtruong711@gmail.com>
2024-01-17 21:57:27 +0700

    Tidy up.

--- src/_auth/Authlayout.tsx
+++ src/_auth/Authlayout.tsx
@@ -6,17 +6,17 @@ const Authlayout = () => {
   return (
     <>
       {isAuthenticated ? (
-        <Navigate to="/" />
+        <Navigate to='/' />
       ): (
         <>
-          <section className="flex flex-1 justify-center items-center flex-col py-10">
+          <section className='flex flex-1 justify-center items-center flex-col py-10'>
             <Outlet />
           </section>
 
           <img
-            src="/assets/images/side-img.svg"
-            alt="logo"
-            className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+            src='/assets/images/side-img.svg'
+            alt='logo'
+            className='hidden xl:block h-screen w-1/2 object-cover bg-no-repeat'
           />
         </>
         
