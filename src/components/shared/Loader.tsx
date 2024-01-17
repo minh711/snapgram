4d4e5204ece4f5baf6a333b2d0ca895872af160a
minh711 <duyminhtruong711@gmail.com>
2024-01-17 22:03:32 +0700

    Tidy up.

--- src/components/shared/Loader.tsx
+++ src/components/shared/Loader.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
-
 const Loader = () => {
   return (
-    <div className="flex-center w-full">
+    <div className='flex-center w-full'>
       <img
-        src="/assets/icons/loader.svg"
-        alt="loader"
+        src='/assets/icons/loader.svg'
+        alt='loader'
         width={24}
         height={24}
       />
