afbfe0301ba33d15e7bb108711099bf0c2786245
minh711 <duyminhtruong711@gmail.com>
2024-01-17 22:03:13 +0700

    Tidy up.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from 'react-router-dom';
-
 import { Home } from './_root/pages';
 import SigninForm from './_auth/forms/SigninForm';
 import SignupForm from './_auth/forms/SignupForm';
@@ -10,15 +9,14 @@ import { Toaster } from '@/components/ui/toaster';
 
 const App = () => {
   return (
-    <main className="flex h-screen">
+    <main className='flex h-screen'>
       <Routes>
         {/* public routes */}
         <Route element={<Authlayout />}>
-          <Route path="/sign-in" element={<SigninForm />} />
-          <Route path="/sign-up" element={<SignupForm />} />
+          <Route path='/sign-in' element={<SigninForm />} />
+          <Route path='/sign-up' element={<SignupForm />} />
         </Route>
         
-
         {/* public routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />  
