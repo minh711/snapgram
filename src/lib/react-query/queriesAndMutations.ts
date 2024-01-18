df007eec105263f2d918085dc868ee0d233206b9
minh711 <duyminhtruong711@gmail.com>
2024-01-18 10:32:59 +0700

    Create post feature complete version 1.

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -1,11 +1,7 @@
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  useInfiniteQuery,
-} from '@tanstack/react-query';
-import { INewUser } from '@/types';
-import { createUserAccount, signInAccount, signOutAccount } from '../appwrite/api';
+import { useQuery, useMutation, useQueryClient, useInfiniteQuery, } from '@tanstack/react-query';
+import { INewPost, INewUser } from '@/types';
+import { createPost, createUserAccount, signInAccount, signOutAccount } from '../appwrite/api';
+import { QUERY_KEYS } from './queryKeys';
 
 export const useCreateUserAccount = () => {
   return useMutation({
@@ -25,5 +21,17 @@ export const useSignInAccount = () => {
 export const useSignOutAccount = () => {
   return useMutation({
     mutationFn: signOutAccount
+  });
+}
+
+export const useCreatePost = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (post: INewPost) => createPost(post),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+      });
+    },
   });
 }
